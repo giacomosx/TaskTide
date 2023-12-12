4fcdc2782e05ff269c538560225e1308aaec5dc2
giacomosx <giacomo.bartoli@me.com>
2023-12-12 23:10:03 +0100

    Delete add_data.py

--- add_data.py
+++
@@ -1,7 +0,0 @@
-from app import db
-from models import Project
-import datetime
-
-new_item = Project(creation_date=datetime.date.today(), description='First Project')
-db.session.add(new_item)
-db.session.commit()
